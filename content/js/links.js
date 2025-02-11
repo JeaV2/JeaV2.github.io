1c59b75d9c4864a52cadfcc382e0fca630111797
JeaV2 <102710@glr.nl>
2025-02-11 18:01:40 +0100

    Update links.js

--- content/js/links.js
+++ content/js/links.js
@@ -2,7 +2,7 @@ const buttonContainer = document.getElementById('buttonContainer');
 if (buttonContainer) {
     buttonContainer.innerHTML = `
         <a href="../" class="button">Home</a>
-        <a href="../profiel/" class="button">Profiel</a>
+        <a href="../profiel/" class="button">Over mij</a>
         <a href="../techniek/" class="button">Technieken</a>
         <a href="../verbetering/" class="button">verbeter punten</a>
         <a href="../interesse/" class="button">Interesse</a>
