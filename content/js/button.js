b5c28161e5f4c1e564651567c749f0eac3c448c7
JeaV2 <102710@glr.nl>
2025-04-04 14:15:38 +0200

    Make mobile menu

--- content/js/button.js
+++ content/js/button.js
@@ -3,16 +3,12 @@ const currentPage = url[url.length - 2] || 'index';
 const buttons = document.getElementById('buttonContainer');
 const aTags = buttons.getElementsByTagName('a');
 
-// console.log('url', url);
-// console.log('Current Page:', currentPage);
-// console.log('Anchor Tags:', aTags);
 
 for (let i = 0; i < aTags.length; i++) {
     const href = aTags[i].getAttribute('href').split('/')[1] || 'index';
-    console.log('Href:', href);
 
     if (href === currentPage) {
-        aTags[i].id = 'currentButton';
+        aTags[i].className += ' currentButton';
         break;
     }
 }
